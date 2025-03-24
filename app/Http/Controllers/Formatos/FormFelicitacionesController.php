28bb991644a5961d00c7f7d3ee4fabbc39d096a8
DiegoSot0 <diego.soto3003@gmail.com>
2025-03-24 10:38:25 -0500

    Arreglo de Felicitaciones sobre asesores que comparten MAC

--- app/Http/Controllers/Formatos/FormFelicitacionesController.php
+++ app/Http/Controllers/Formatos/FormFelicitacionesController.php
@@ -15,7 +15,8 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class FormFelicitacionesController extends Controller
 {
-    private function centro_mac(){
+    private function centro_mac()
+    {
         // VERIFICAMOS EL USUARIO A QUE CENTRO MAC PERTENECE
         /*================================================================================================================*/
         $us_id = auth()->user()->idcentro_mac;
@@ -25,7 +26,7 @@ class FormFelicitacionesController extends Controller
         $name_mac = $user->NOMBRE_MAC;
         /*================================================================================================================*/
 
-        $resp = ['idmac'=>$idmac, 'name_mac'=>$name_mac ];
+        $resp = ['idmac' => $idmac, 'name_mac' => $name_mac];
 
         return (object) $resp;
     }
@@ -38,37 +39,36 @@ class FormFelicitacionesController extends Controller
     public function tb_index(Request $request)
     {
         $query = FLibroFelicitacion::from('F_LIBRO_FELICITACIONES as FLF')->select(
-                                            'FLF.IDLIBRO_FELICITACION',
-                                            'FLF.CORRELATVIO',
-                                            'FLF.R_FECHA',
-                                            'MCM.NOMBRE_MAC',
-                                            \DB::raw('MPR.NOMBRE AS NOM_REGISTRA'),
-                                            \DB::raw('CONCAT(FLF.R_NOMBRE, \', \', FLF.R_APE_PAT, \' \', FLF.R_APE_MAT) AS NOMBREU'),
-                                            \DB::raw('CONCAT(DPT.TIPODOC_ABREV, \' - \', FLF.R_NUM_DOC) AS DOCUMENTO'),
-                                            'FLF.R_DESCRIPCION',
-                                            'ME.ABREV_ENTIDAD',
-                                            \DB::raw('CONCAT(MP.NOMBRE, \', \', MP.APE_PAT, \' \', MP.APE_MAT) AS ASESOR'),
-                                            'FLF.R_ARCHIVO_RUT',
-                                            'FLF.R_ARCHIVO_NOM',
-                                            'FLF.AÑO',
-                                            'FLF.CORRELATIVO_MAC',
-                                        )
-                                            ->leftJoin('M_PERSONAL AS MP', 'MP.IDPERSONAL', '=', 'FLF.IDPERSONAL')
-                                            ->leftJoin('M_PERSONAL AS MPR', 'MPR.IDPERSONAL', '=', 'FLF.IDPER_REGISTRA')
-                                            ->leftJoin('M_CENTRO_MAC AS MCM', 'MCM.IDCENTRO_MAC', '=', 'FLF.IDCENTRO_MAC')
-                                            ->leftJoin('D_PERSONAL_TIPODOC AS DPT', 'DPT.IDTIPO_DOC', '=', 'FLF.IDTIPO_DOC')
-                                            ->leftJoin('M_ENTIDAD AS ME', 'ME.IDENTIDAD', '=', 'FLF.IDENTIDAD')
-                                            ->where('FLF.FLAG', 1)
-                                            ->where(function($query) {
-                                                if (auth()->user()->hasRole('Especialista TIC|Orientador|Asesor|Supervisor|Coordinador')) {
-                                                    $query->where('FLF.IDCENTRO_MAC', '=', $this->centro_mac()->idmac);
-                                                }
-                                            })
-                                            ->orderBy('FLF.CORRELATVIO', 'desc')                                            
-                                            ->get();
+            'FLF.IDLIBRO_FELICITACION',
+            'FLF.CORRELATVIO',
+            'FLF.R_FECHA',
+            'MCM.NOMBRE_MAC',
+            \DB::raw('MPR.NOMBRE AS NOM_REGISTRA'),
+            \DB::raw('CONCAT(FLF.R_NOMBRE, \', \', FLF.R_APE_PAT, \' \', FLF.R_APE_MAT) AS NOMBREU'),
+            \DB::raw('CONCAT(DPT.TIPODOC_ABREV, \' - \', FLF.R_NUM_DOC) AS DOCUMENTO'),
+            'FLF.R_DESCRIPCION',
+            'ME.ABREV_ENTIDAD',
+            \DB::raw('CONCAT(MP.NOMBRE, \', \', MP.APE_PAT, \' \', MP.APE_MAT) AS ASESOR'),
+            'FLF.R_ARCHIVO_RUT',
+            'FLF.R_ARCHIVO_NOM',
+            'FLF.AÑO',
+            'FLF.CORRELATIVO_MAC',
+        )
+            ->leftJoin('M_PERSONAL AS MP', 'MP.IDPERSONAL', '=', 'FLF.IDPERSONAL')
+            ->leftJoin('M_PERSONAL AS MPR', 'MPR.IDPERSONAL', '=', 'FLF.IDPER_REGISTRA')
+            ->leftJoin('M_CENTRO_MAC AS MCM', 'MCM.IDCENTRO_MAC', '=', 'FLF.IDCENTRO_MAC')
+            ->leftJoin('D_PERSONAL_TIPODOC AS DPT', 'DPT.IDTIPO_DOC', '=', 'FLF.IDTIPO_DOC')
+            ->leftJoin('M_ENTIDAD AS ME', 'ME.IDENTIDAD', '=', 'FLF.IDENTIDAD')
+            ->where('FLF.FLAG', 1)
+            ->where(function ($query) {
+                if (auth()->user()->hasRole('Especialista TIC|Orientador|Asesor|Supervisor|Coordinador')) {
+                    $query->where('FLF.IDCENTRO_MAC', '=', $this->centro_mac()->idmac);
+                }
+            })
+            ->orderBy('FLF.CORRELATVIO', 'desc')
+            ->get();
 
         return view('formatos.f_felicitaciones.tablas.tb_index', compact('query'));
-
     }
 
     public function md_add_felicitacion(Request $request)
@@ -76,13 +76,20 @@ class FormFelicitacionesController extends Controller
         $tip_doc = DB::table('D_PERSONAL_TIPODOC')->get();
 
         $entidad = DB::table('M_MAC_ENTIDAD')
-                            ->join('M_CENTRO_MAC', 'M_CENTRO_MAC.IDCENTRO_MAC', '=', 'M_MAC_ENTIDAD.IDCENTRO_MAC')
-                            ->join('M_ENTIDAD', 'M_ENTIDAD.IDENTIDAD', '=', 'M_MAC_ENTIDAD.IDENTIDAD')
-                            ->where('M_MAC_ENTIDAD.IDCENTRO_MAC', $this->centro_mac()->idmac)
-                            ->get();
+            ->join('M_CENTRO_MAC', 'M_CENTRO_MAC.IDCENTRO_MAC', '=', 'M_MAC_ENTIDAD.IDCENTRO_MAC')
+            ->join('M_ENTIDAD', 'M_ENTIDAD.IDENTIDAD', '=', 'M_MAC_ENTIDAD.IDENTIDAD')
+            ->where('M_MAC_ENTIDAD.IDCENTRO_MAC', $this->centro_mac()->idmac)
+            ->get();
 
-        $asesor = Personal::where('FLAG' , 1)->where('IDMAC', $this->centro_mac()->idmac)->get();
-
+        # $asesor = Personal::where('FLAG' , 1)->where('IDMAC', $this->centro_mac()->idmac)->get();
+        // Obtención de los asesores filtrados por el status = 1 y el idmac del centro MAC actual
+        $asesor = DB::table('D_PERSONAL_MAC')
+            ->join('M_PERSONAL', 'D_PERSONAL_MAC.IDPERSONAL', '=', 'M_PERSONAL.IDPERSONAL')
+            ->where('D_PERSONAL_MAC.Status', 1) // Filtra por status = 1
+            ->where('D_PERSONAL_MAC.IDCENTRO_MAC', $this->centro_mac()->idmac) // Filtra por el centro MAC actual
+            ->select('M_PERSONAL.IDPERSONAL', 'M_PERSONAL.NOMBRE', 'M_PERSONAL.APE_PAT', 'M_PERSONAL.APE_MAT')
+            ->get();
+            
         $view = view('formatos.f_felicitaciones.modals.md_add_felicitacion', compact('tip_doc', 'entidad', 'asesor'))->render();
 
         return response()->json(['html' => $view]);
@@ -95,21 +102,21 @@ class FormFelicitacionesController extends Controller
             $currentYear = Carbon::now()->format('Y');
 
             $corr_ =  FLibroFelicitacion::where('AÑO', $currentYear)
-                                ->orderby('IDLIBRO_FELICITACION', 'DESC')
-                                ->first();
+                ->orderby('IDLIBRO_FELICITACION', 'DESC')
+                ->first();
 
-            if(isset($corr_->CORRELATVIO)){
+            if (isset($corr_->CORRELATVIO)) {
                 $cont_ = $corr_->CORRELATVIO + 1;
                 // dd($cont_);
                 $codpadron = Str::padLeft($cont_, 7, '0');
-            }else{
+            } else {
                 $codpadron = '0000001';
             }
 
 
             $num_doc = $request->num_doc;
 
-            $estructura_carp = 'formato_archivo\\felicitaciones\\'.$num_doc;
+            $estructura_carp = 'formato_archivo\\felicitaciones\\' . $num_doc;
 
             if (!file_exists($estructura_carp)) {
                 mkdir($estructura_carp, 0777, true);
@@ -132,8 +139,7 @@ class FormFelicitacionesController extends Controller
             $save->R_DESCRIPCION = $request->descripcion;
             $save->IDENTIDAD = $request->entidad;
             $save->IDPERSONAL = $request->asesor;
-            if($request->hasFile('file_doc'))
-            {
+            if ($request->hasFile('file_doc')) {
                 $archivoPDF = $request->file('file_doc');
                 $nombrePDF = $archivoPDF->getClientOriginalName();
                 //$nameruta = '/img/fotoempresa/'; // RUTA DONDE SE VA ALMACENAR EL DOCUMENTO PDF
@@ -146,8 +152,7 @@ class FormFelicitacionesController extends Controller
             $save->save();
 
             return $save;
-
-        }catch (\Exception $e) {
+        } catch (\Exception $e) {
             //Si existe algún error en la Transacción
             $response_ = response()->json([
                 'data' => null,
@@ -164,12 +169,12 @@ class FormFelicitacionesController extends Controller
         $tip_doc = DB::table('D_PERSONAL_TIPODOC')->get();
 
         $entidad = DB::table('M_MAC_ENTIDAD')
-                            ->join('M_CENTRO_MAC', 'M_CENTRO_MAC.IDCENTRO_MAC', '=', 'M_MAC_ENTIDAD.IDCENTRO_MAC')
-                            ->join('M_ENTIDAD', 'M_ENTIDAD.IDENTIDAD', '=', 'M_MAC_ENTIDAD.IDENTIDAD')
-                            ->where('M_MAC_ENTIDAD.IDCENTRO_MAC', $this->centro_mac()->idmac)
-                            ->get();
+            ->join('M_CENTRO_MAC', 'M_CENTRO_MAC.IDCENTRO_MAC', '=', 'M_MAC_ENTIDAD.IDCENTRO_MAC')
+            ->join('M_ENTIDAD', 'M_ENTIDAD.IDENTIDAD', '=', 'M_MAC_ENTIDAD.IDENTIDAD')
+            ->where('M_MAC_ENTIDAD.IDCENTRO_MAC', $this->centro_mac()->idmac)
+            ->get();
 
-        $asesor = Personal::where('FLAG' , 1)->where('IDMAC', $this->centro_mac()->idmac)->orderBy('APE_PAT', 'ASC')->get();
+        $asesor = Personal::where('FLAG', 1)->where('IDMAC', $this->centro_mac()->idmac)->orderBy('APE_PAT', 'ASC')->get();
 
         $felicitacion = FLibroFelicitacion::where('IDLIBRO_FELICITACION', $request->idfelicitacion)->first();
 
@@ -183,7 +188,7 @@ class FormFelicitacionesController extends Controller
         try {
             $num_doc = $request->num_doc;
 
-            $estructura_carp = 'formato_archivo\\felicitaciones\\'.$num_doc;
+            $estructura_carp = 'formato_archivo\\felicitaciones\\' . $num_doc;
 
             if (!file_exists($estructura_carp)) {
                 mkdir($estructura_carp, 0777, true);
@@ -199,8 +204,7 @@ class FormFelicitacionesController extends Controller
             $save->R_DESCRIPCION = $request->descripcion;
             $save->IDENTIDAD = $request->entidad;
             $save->IDPERSONAL = $request->asesor;
-            if($request->hasFile('file_doc'))
-            {
+            if ($request->hasFile('file_doc')) {
                 $archivoPDF = $request->file('file_doc');
                 $nombrePDF = $archivoPDF->getClientOriginalName();
                 //$nameruta = '/img/fotoempresa/'; // RUTA DONDE SE VA ALMACENAR EL DOCUMENTO PDF
@@ -213,8 +217,7 @@ class FormFelicitacionesController extends Controller
             $save->save();
 
             return $save;
-
-        }catch (\Exception $e) {
+        } catch (\Exception $e) {
             //Si existe algún error en la Transacción
             $response_ = response()->json([
                 'data' => null,
@@ -231,12 +234,12 @@ class FormFelicitacionesController extends Controller
         $archivo = FLibroFelicitacion::where('IDLIBRO_FELICITACION', $request->idfelicitacion)->first();
 
         $del = FLibroFelicitacion::where('IDLIBRO_FELICITACION', $request->idfelicitacion)->update([
-                                                                                                        'R_ARCHIVO_NOM' => null,
-                                                                                                        'R_ARCHIVO_RUT' => null,
-                                                                                                    ]);
+            'R_ARCHIVO_NOM' => null,
+            'R_ARCHIVO_RUT' => null,
+        ]);
 
-        if(file_exists( $archivo->R_ARCHIVO_RUT.'/'.$archivo->R_ARCHIVO_NOM)){
-            unlink($archivo->R_ARCHIVO_RUT.'/'.$archivo->R_ARCHIVO_NOM);
+        if (file_exists($archivo->R_ARCHIVO_RUT . '/' . $archivo->R_ARCHIVO_NOM)) {
+            unlink($archivo->R_ARCHIVO_RUT . '/' . $archivo->R_ARCHIVO_NOM);
         }
 
         return $del;
@@ -272,35 +275,34 @@ class FormFelicitacionesController extends Controller
     public function export_excel(Request $request)
     {
         $query = FLibroFelicitacion::from('F_LIBRO_FELICITACIONES as FLF')->select(
-                                                'FLF.IDLIBRO_FELICITACION',
-                                                'FLF.CORRELATVIO',
-                                                'FLF.R_FECHA',
-                                                'MCM.NOMBRE_MAC',
-                                                \DB::raw('MPR.NOMBRE AS NOM_REGISTRA'),
-                                                \DB::raw('CONCAT(FLF.R_NOMBRE, \', \', FLF.R_APE_PAT, \' \', FLF.R_APE_MAT) AS NOMBREU'),
-                                                \DB::raw('CONCAT(DPT.TIPODOC_ABREV, \' - \', FLF.R_NUM_DOC) AS DOCUMENTO'),
-                                                'FLF.R_DESCRIPCION',
-                                                'ME.ABREV_ENTIDAD',
-                                                \DB::raw('CONCAT(MP.NOMBRE, \', \', MP.APE_PAT, \' \', MP.APE_MAT) AS ASESOR'),
-                                                'FLF.R_ARCHIVO_RUT',
-                                                'FLF.R_ARCHIVO_NOM',
-                                                'FLF.AÑO',
-                                                'FLF.CORRELATIVO_MAC',
-                                            )
-                                        ->leftJoin('M_PERSONAL AS MP', 'MP.IDPERSONAL', '=', 'FLF.IDPERSONAL')
-                                        ->leftJoin('M_PERSONAL AS MPR', 'MPR.IDPERSONAL', '=', 'FLF.IDPER_REGISTRA')
-                                        ->leftJoin('M_CENTRO_MAC AS MCM', 'MCM.IDCENTRO_MAC', '=', 'FLF.IDCENTRO_MAC')
-                                        ->leftJoin('D_PERSONAL_TIPODOC AS DPT', 'DPT.IDTIPO_DOC', '=', 'FLF.IDTIPO_DOC')
-                                        ->leftJoin('M_ENTIDAD AS ME', 'ME.IDENTIDAD', '=', 'FLF.IDENTIDAD')
-                                        ->where('FLF.FLAG', 1)
-                                        ->orderBy('FLF.CORRELATVIO', 'desc')
-                                        ->get();
+            'FLF.IDLIBRO_FELICITACION',
+            'FLF.CORRELATVIO',
+            'FLF.R_FECHA',
+            'MCM.NOMBRE_MAC',
+            \DB::raw('MPR.NOMBRE AS NOM_REGISTRA'),
+            \DB::raw('CONCAT(FLF.R_NOMBRE, \', \', FLF.R_APE_PAT, \' \', FLF.R_APE_MAT) AS NOMBREU'),
+            \DB::raw('CONCAT(DPT.TIPODOC_ABREV, \' - \', FLF.R_NUM_DOC) AS DOCUMENTO'),
+            'FLF.R_DESCRIPCION',
+            'ME.ABREV_ENTIDAD',
+            \DB::raw('CONCAT(MP.NOMBRE, \', \', MP.APE_PAT, \' \', MP.APE_MAT) AS ASESOR'),
+            'FLF.R_ARCHIVO_RUT',
+            'FLF.R_ARCHIVO_NOM',
+            'FLF.AÑO',
+            'FLF.CORRELATIVO_MAC',
+        )
+            ->leftJoin('M_PERSONAL AS MP', 'MP.IDPERSONAL', '=', 'FLF.IDPERSONAL')
+            ->leftJoin('M_PERSONAL AS MPR', 'MPR.IDPERSONAL', '=', 'FLF.IDPER_REGISTRA')
+            ->leftJoin('M_CENTRO_MAC AS MCM', 'MCM.IDCENTRO_MAC', '=', 'FLF.IDCENTRO_MAC')
+            ->leftJoin('D_PERSONAL_TIPODOC AS DPT', 'DPT.IDTIPO_DOC', '=', 'FLF.IDTIPO_DOC')
+            ->leftJoin('M_ENTIDAD AS ME', 'ME.IDENTIDAD', '=', 'FLF.IDENTIDAD')
+            ->where('FLF.FLAG', 1)
+            ->orderBy('FLF.CORRELATVIO', 'desc')
+            ->get();
 
         $name_mac = $this->centro_mac()->name_mac;
 
-        $export = Excel::download(new FelicitacionExport($query, $name_mac), 'FORMATO LIBRO DE FELICITACIONES  CENTRO MAC - '.$this->centro_mac()->name_mac.'.xlsx');
+        $export = Excel::download(new FelicitacionExport($query, $name_mac), 'FORMATO LIBRO DE FELICITACIONES  CENTRO MAC - ' . $this->centro_mac()->name_mac . '.xlsx');
 
         return $export;
-
     }
 }
